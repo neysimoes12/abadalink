435f467452f6c61136a3c37a5ab1ca798a4cc6e4
Ney <ney>
2026-02-07 07:35:56 -0300

    Fix 502: Use python execution with app object

--- main.py
+++ main.py
@@ -1077,4 +1077,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     print(f"Starting app on port {port}")
-    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=port, workers=1)
